210cd00971376ea0c81727c662bed7c1766c4ffd
am185238 <alex.munoz@ncr.com>
2018-04-26 08:56:31 -0700

    library major refactor - add logging bridge for chef/standalone compatibility

--- files/sql_helper.ps1
+++ files/sql_helper.ps1
@@ -1,96 +1,60 @@
-# PowerShell v2.0 compatible version of [string]::IsNullOrWhitespace.
-function StringIsNullOrWhitespace([string] $string)
-{
-    if ($string -ne $null) { $string = $string.Trim() }
-    return [string]::IsNullOrEmpty($string)
-}
+function Invoke-SQL {
+  param(
+      [string] $connectionString = $(throw "Please specify a connection string"),
+      [string] $sqlCommand = $(throw "Please specify a query.")
+    )
 
-# Returns row(s) affected
-function ExecuteNonQuery {
-param(
-    [Parameter(Mandatory=$True,Position=1)] [string]$cnstr,
-    [Parameter(Mandatory=$True,Position=2)] [string]$query,
-    [Parameter(Mandatory=$False,Position=3)] [bool]$showOutput = $true,
-    [Parameter(Mandatory=$False,Position=4)] [bool]$ignoreErrors = $false
-)
-  return ExecuteQuery $cnstr $query 'nonquery' $showOutput
-}
-
-# Returns scalar value
-function ExecuteScalar {
-param(
-    [Parameter(Mandatory=$True,Position=1)] [string]$cnstr,
-    [Parameter(Mandatory=$True,Position=2)] [string]$query,
-    [Parameter(Mandatory=$False,Position=3)] [bool]$showOutput = $true,
-    [Parameter(Mandatory=$False,Position=4)] [bool]$ignoreErrors = $false
-)
-  return ExecuteQuery $cnstr $query 'scalar' $showOutput
-}
-
-# Returns first row found in results
-function ExecuteReader {
-param(
-    [Parameter(Mandatory=$True,Position=1)] [string]$cnstr,
-    [Parameter(Mandatory=$True,Position=2)] [string]$query,
-    [Parameter(Mandatory=$False,Position=3)] [bool]$showOutput = $true,
-    [Parameter(Mandatory=$False,Position=4)] [bool]$ignoreErrors = $false
-)
-  return ExecuteQuery $cnstr $query 'reader' $showOutput
-}
-
-# Returns scalar value or row(s) affected
-function ExecuteQuery {
-param(
-    [Parameter(Mandatory=$True,Position=1)] [string]$cnstr,
-    [Parameter(Mandatory=$True,Position=2)] [string]$query,
-    [Parameter(Mandatory=$True,Position=3)] [string]$queryType,
-    [Parameter(Mandatory=$False,Position=4)] [bool]$showOutput = $true,
-    [Parameter(Mandatory=$False,Position=5)] [bool]$ignoreErrors = $false
-)
-  $Connection = New-Object System.Data.SQLClient.SQLConnection
-  $Connection.ConnectionString = $cnstr
-
-  # Attach the InfoMessage Event Handler to the connection if ignoring errors
-  if ($ignoreErrors -eq $true) {
-    $handler = [System.Data.SqlClient.SqlInfoMessageEventHandler] {param($sender, $event) Write-Host $event.Message };
-    $Connection.add_InfoMessage($handler);
-    $Connection.FireInfoMessageEventOnUserErrors = $false;
-  }
-
-  # Let chef raise any exceptions
+  $sqlCommands = $sqlCommand -split "\n\s*GO\s*\n" # Split the query on each GO statement.
+  $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
+  $command = new-object system.data.sqlclient.sqlcommand($sqlCommand,$connection)
+  $adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
   $connection.Open()
-  $queries = $query -split "\n\s*GO\s*\n" # Split the query on each GO statement.
-  $command = New-Object System.Data.SQLClient.SQLCommand
-  $command.Connection = $connection
-  foreach ($currentQuery in $queries) {
-    $command.CommandText = $currentQuery
-    if (StringIsNullOrWhitespace($command.CommandText)) { continue }
-    if ($queryType -eq 'scalar') {
-      $result = '[ { ''result'' => ''' + $command.ExecuteScalar() + ''' } ]'
-    } elseif ($queryType -eq 'reader') {
-      $reader = $command.ExecuteReader()
-      $result = "[ "
-      while ($reader.Read()) {
-        $result = "$result { "
-        for ($i = 0;$i -lt $reader.FieldCount; $i++) {
-          $columnName = $reader.GetName($i)
-          if ($reader.IsDBNull($i)) {
-            $columnValue = 'null'
-          } else {
-            $columnValue = $reader.GetValue($i)
-          }
-          $result = "$result '$columnName' => '$columnValue',"
-        }
-        $result = $result.Substring(0, $result.Length - 1)
-        $result = "$result },"
-      }
-      $result = $result.Substring(0, $result.Length - 1)
-      $result = "$result ]"
-    } else {
-      $result = '[ { ''result'' => ''' + $command.ExecuteNonQuery() + ''' } ]'
-    }
-  }
-  $Connection.Close()
 
-  return $result
+  foreach ($sqlcmd in $sqlCommands) {
+    if ([string]::IsNullOrEmpty($sqlcmd)) { continue }
+    $command.CommandText = $sqlcmd
+    $dataset = New-Object System.Data.DataSet
+    $adapter.Fill($dataSet) | Out-Null
+    $tables = $tables + $dataSet.Tables
+  }
+
+  $connection.Close()
+  $tables
+}
+
+function ConvertSqlDatasetTo-Json {
+  param(
+      [object] $dataset = $(throw "Please specify a dataset")
+    )
+
+  $convertedTables = '{ '
+  foreach ($table in $dataset.DataSet.Tables) {
+    $convertedTable = ($table | select $table.Columns.ColumnName) | ConvertTo-Json -Compress
+    if (!$convertedTable) { $convertedTable = '[]' }
+    if (!$convertedTable.StartsWith('[')) { $convertedTable = "[ $convertedTable ]" } # Convert to Array if it's not
+    $convertedTables += '"' + $table.TableName + '": ' + $convertedTable + ','
+  }
+  $convertedTables.TrimEnd(',') + ' }'
+}
+
+function Build-ConnectionString{
+  param(
+      [string] $vip = $(throw "Please specify a server vip"),
+      [int] $port,
+      [string] $database = $(throw "Please specify a database"),
+      [string] $username,
+      [string] $password
+  )
+  $target = $vip
+  if($port -ne $null -and $port -ne 0){
+      $target = "$target,$port"
+  }
+  if(($username -ne "") -and ($password -ne "")){
+      $credentials = "Integrated Security=False;User ID=$username;Password=$password"
+  }
+  else{
+      Write-Warning "no credentials provided.  falling back to integrated security"
+      $credentials = "Integrated Security=True;"
+  }
+  return "Data Source=$target; Initial Catalog=$database; $credentials"
 }
---
+++ libraries/chef_bridge.rb
@@ -0,0 +1,64 @@
+module IOHelper
+  class ChefLogger
+    def info(message)
+      Chef::Log.info(message)
+    end
+
+    def debug(message)
+      Chef::Log.debug(message)
+    end
+
+    def warn(message)
+      Chef::Log.warn(message)
+    end
+
+    def fatal(message)
+      Chef::Log.fatal(message)
+    end
+  end
+
+  @logger = ChefLogger.new
+
+  def self.logger
+    @logger
+  end
+end
+
+module Settings
+  require 'pp' if Chef::Config[:log_level] == :debug
+
+  @script_dir = File.expand_path(File.dirname(__FILE__))
+
+  def self.environment
+    {
+      'datacenter' => '',
+      'policy_group' => 'local',
+      'datacenters' => %w()
+    }
+  end
+
+  def self.paths
+    {
+      'cache_path' => "#{Chef::Config[:file_cache_path]}/sql_helper",
+      'always_on_backup_temp_dir' => '',
+      'script_dir' => "#{@script_dir.tr('/', '\\')}\\..\\files",
+      'replication_scripts' => "#{Chef::Config[:file_cache_path]}/sql_helper/replication",
+      'logins_export_folder' => "#{Chef::Config[:file_cache_path]}/sql_helper/logins",
+    }
+  end
+
+  def self.backups
+    {
+      'free_space_threshold' => 15, # minimum percent free space after backup required for primary destination
+      'alternate_destination' => '', # This should be a UNC path. In most cases this is a network share with ample space
+      'default_backup_share' => 'SqlBackup', # The default share to check first when looking for backup files on a server
+      'compress_backups' => false,
+    }
+  end
+
+  def self.exports
+    {
+      'include_table_permissions' => false,
+    }
+  end
+end
